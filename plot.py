f2f2079991ec3caa48486506d3b6cd51baff0005
Andrzej <novak5andrzej@gmail.com>
2019-10-29 18:05:07 -0500

    Add temp models

--- plot.py
+++ plot.py
@@ -19,7 +19,8 @@ parser.add_argument("-i",
                     default='hxxModel/fitDiagnostics.root',
                     help="Input fitDiagnostics file")
 parser.add_argument("--space",
-                    default='prefit', dest='namespace',
+                    default='prefit',
+                    dest='namespace',
                     help="fitDiagnostics namespace to plot")
 args = parser.parse_args()
 
@@ -66,10 +67,12 @@ label_dict = OrderedDict({
     'tqq': "$\mathrm{t\\bar{t}}$",
 })
 
+
 def full_plot(cats, pseudo=True):
     class Ugh():
         def __init__(self):
             self.plot_bins = None
+
     ugh = Ugh()
 
     def tgasym_to_err(tgasym):
@@ -82,15 +85,14 @@ def full_plot(cats, pseudo=True):
         _yerrlo, _yerrhi = tgasym._fEYlow * _binwidth, tgasym._fEYhigh * _binwidth
         return _x, _y, _yerrlo, _yerrhi, _xerrlo, _xerrhi
 
-
     def plot_data(x, y, yerr, xerr, ax=None, pseudo=pseudo, ugh=None):
         if ugh is None:
             ugh = Ugh()
         data_err_opts = {
-            'linestyle':'none',
+            'linestyle': 'none',
             'marker': '.',
             'markersize': 12.,
-            'color':'k',
+            'color': 'k',
             'elinewidth': 2,
         }
         if np.sum([y != 0][0]) > 0:
@@ -100,39 +102,57 @@ def full_plot(cats, pseudo=True):
                 ugh.plot_bins = (ugh.plot_bins & [y != 0][0])
         x = np.array(x)[ugh.plot_bins]
         y = np.array(y)[ugh.plot_bins]
-        yerr = [np.array(yerr[0])[ugh.plot_bins], np.array(yerr[1])[ugh.plot_bins]]
-        xerr = [np.array(xerr)[0][ugh.plot_bins], np.array(xerr)[1][ugh.plot_bins]]
-        ax.errorbar(x, y, yerr, xerr, fmt='+', label="MC" if pseudo else "Data", **data_err_opts)
-
+        yerr = [
+            np.array(yerr[0])[ugh.plot_bins],
+            np.array(yerr[1])[ugh.plot_bins]
+        ]
+        xerr = [
+            np.array(xerr)[0][ugh.plot_bins],
+            np.array(xerr)[1][ugh.plot_bins]
+        ]
+        ax.errorbar(x,
+                    y,
+                    yerr,
+                    xerr,
+                    fmt='+',
+                    label="MC" if pseudo else "Data",
+                    **data_err_opts)
 
     def th1_to_step(th1):
         _h, _bins = th1.numpy()
-        _binwidths = [_bins[i+1] - _bins[i] for i in range(len(_bins[:-1]))]
+        _binwidths = [_bins[i + 1] - _bins[i] for i in range(len(_bins[:-1]))]
         _h *= _binwidths
         return _bins, np.r_[_h, _h[-1]]
-
 
     def plot_step(bins, h, ax=None, label=None, nozeros=True, **kwargs):
         ax.step(bins, h, where='post', label=label, c=cdict[label], **kwargs)
 
+    avail_samples = [
+        k.decode(encoding="utf-8").split(';')[0] for k in cats[0].keys()
+        if b'total' not in k
+    ]
 
     # Plotting
-    fig, (ax, rax) = plt.subplots(2,1, gridspec_kw={'height_ratios': (3,1)}, sharex=True)
-    plt.subplots_adjust(hspace = 0)
+    fig, (ax, rax) = plt.subplots(2,
+                                  1,
+                                  gridspec_kw={'height_ratios': (3, 1)},
+                                  sharex=True)
+    plt.subplots_adjust(hspace=0)
 
     ## Main
     res = np.array(list(map(tgasym_to_err, [cat['data'] for cat in cats])))
     ### Sum along y, keep only first one for x
-    _x, _y = res[:,0][0], np.sum(res[:,1], axis=0),
-    _yerrlo, _yerrhi  = np.sum(res[:,2], axis=0), np.sum(res[:,3], axis=0)
-    _xerrlo, _xerrhi = res[:,4][0], res[:,5][0]
+    _x, _y = res[:, 0][0], np.sum(res[:, 1], axis=0),
+    _yerrlo, _yerrhi = np.sum(res[:, 2], axis=0), np.sum(res[:, 3], axis=0)
+    _xerrlo, _xerrhi = res[:, 4][0], res[:, 5][0]
     plot_data(_x, _y, [_yerrlo, _yerrhi], [_xerrlo, _xerrhi], ax=ax, ugh=ugh)
 
     ### Stack qcd/ttbar
     tot_h, bins = None, None
-    for mc, zo in zip(['qcd', 'tqq'], [1,0]):
+    for mc, zo in zip(['qcd', 'tqq'], [1, 0]):
+        if mc not in avail_samples: continue
         res = np.array(list(map(th1_to_step, [cat[mc] for cat in cats])))
-        bins, h = res[:,0][0], np.sum(res[:,1], axis=0)
+        bins, h = res[:, 0][0], np.sum(res[:, 1], axis=0)
         if tot_h is None:
             plot_step(bins, h, ax=ax, label=mc, zorder=zo)
             tot_h = h
@@ -143,8 +163,9 @@ def full_plot(cats, pseudo=True):
     ### Stack plots
     tot_h, bins = None, None
     for mc in ['hqq', 'hcc', 'zbb', 'zcc', 'zqq', 'wcq', 'wqq']:
+        if mc not in avail_samples: continue
         res = np.array(list(map(th1_to_step, [cat[mc] for cat in cats])))
-        bins, h = res[:,0][0], np.sum(res[:,1], axis=0)
+        bins, h = res[:, 0][0], np.sum(res[:, 1], axis=0)
         if tot_h is None:
             plot_step(bins, h, ax=ax, label=mc)
             tot_h = h
@@ -158,30 +179,35 @@ def full_plot(cats, pseudo=True):
 
     ### Caculate diff
     res = np.array(list(map(tgasym_to_err, [cat['data'] for cat in cats])))
-    _x, _y = res[:,0][0], np.sum(res[:,1], axis=0),
-    _yerrlo, _yerrhi  = np.sum(res[:,2], axis=0), np.sum(res[:,3], axis=0)
-    _xerrlo, _xerrhi = res[:,4][0], res[:,5][0]
+    _x, _y = res[:, 0][0], np.sum(res[:, 1], axis=0),
+    _yerrlo, _yerrhi = np.sum(res[:, 2], axis=0), np.sum(res[:, 3], axis=0)
+    _xerrlo, _xerrhi = res[:, 4][0], res[:, 5][0]
     #### Subtract MC
     y = np.copy(_y)
     for mc in ['qcd', 'tqq']:
+        if mc not in avail_samples: continue
         res = np.array(list(map(th1_to_step, [cat[mc] for cat in cats])))
-        bins, h = res[:,0][0], np.sum(res[:,1], axis=0)
-        y -= h[:-1] # Last step duplicate is not needed
+        bins, h = res[:, 0][0], np.sum(res[:, 1], axis=0)
+        y -= h[:-1]  # Last step duplicate is not needed
     #### Scale by data uncertainty
     y /= (_yerrlo + _yerrhi)
-    plot_data(_x, y, [_yerrlo/_yerrlo, _yerrhi/_yerrhi], [_xerrlo, _xerrhi], ax=rax, ugh=ugh)
+    plot_data(_x,
+              y, [_yerrlo / _yerrlo, _yerrhi / _yerrhi], [_xerrlo, _xerrhi],
+              ax=rax,
+              ugh=ugh)
     _scale_for_mc = np.r_[(_yerrlo + _yerrhi), (_yerrlo + _yerrhi)[-1]]
 
     ### Stack plots
     tot_h, bins = None, None
     for mc in ['hqq', 'hcc', 'zbb', 'zcc', 'zqq', 'wcq', 'wqq']:
+        if mc not in avail_samples: continue
         res = np.array(list(map(th1_to_step, [cat[mc] for cat in cats])))
-        bins, h = res[:,0][0], np.sum(res[:,1], axis=0)
+        bins, h = res[:, 0][0], np.sum(res[:, 1], axis=0)
         if tot_h is None:
-            plot_step(bins, h/_scale_for_mc, ax=rax, label=mc)
+            plot_step(bins, h / _scale_for_mc, ax=rax, label=mc)
             tot_h = h
         else:
-            plot_step(bins, (h + tot_h)/_scale_for_mc, label=mc, ax=rax)
+            plot_step(bins, (h + tot_h) / _scale_for_mc, label=mc, ax=rax)
             tot_h += h
 
     ############
@@ -191,48 +217,82 @@ def full_plot(cats, pseudo=True):
 
     ax.set_ylabel('Events / 7GeV', ha='right', y=1)
     rax.set_xlabel('jet $\mathrm{m_{SD}}$ [GeV]', ha='right', x=1)
-    rax.set_ylabel(r'$\mathrm{\frac{Data-(MultiJet+t\bar{t})}{\sigma_{Data}}}$')
+    rax.set_ylabel(
+        r'$\mathrm{\frac{Data-(MultiJet+t\bar{t})}{\sigma_{Data}}}$')
 
-    ax.set_xlim(40,200)
-    ax.set_ylim(0,ax.get_ylim()[1]*1.4)
+    ax.set_xlim(40, 200)
+    ax.set_ylim(0, ax.get_ylim()[1] * 1.4)
     #ax.ticklabel_format(axis='y', style='sci', scilimits=(0,3), useOffset=False)
     #ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.e'))
     f = mtick.ScalarFormatter(useOffset=False, useMathText=True)
-    g = lambda x,pos : "${}$".format(f._formatSciNotation('%1.10e' % x))
+    g = lambda x, pos: "${}$".format(f._formatSciNotation('%1.10e' % x))
     ax.yaxis.set_major_formatter(mtick.FuncFormatter(g))
-    rax.set_ylim(rax.get_ylim()[0]*1.3, rax.get_ylim()[1]*1.3)
+    rax.set_ylim(rax.get_ylim()[0] * 1.3, rax.get_ylim()[1] * 1.3)
 
-    ipt = int(str(cats[0].name, 'utf-8').split('ptbin')[1][0])
-    if len(cats) == 1:
-        pt_range = str(pbins[ipt])+"$< \mathrm{p_T} <$"+str(pbins[ipt+1])+" GeV"
+    ipt = int(str(cats[0].name, 'utf-8').split('ptbin')[1][0]) if b'ptbin' in cats[0].name else 0
+    if len(cats) == 1 or b'muon' not in cats[0].name:
+        pt_range = str(pbins[ipt]) + "$< \mathrm{p_T} <$" + str(
+            pbins[ipt + 1]) + " GeV"
     else:
-        pt_range = str(pbins[0])+"$< \mathrm{p_T} <$"+str(pbins[-1])+" GeV"
+        pt_range = str(pbins[0]) + "$< \mathrm{p_T} <$" + str(
+            pbins[-1]) + " GeV"
+
     annot = pt_range \
-            +'\nDeepDoubleX' \
+            +'\nDeepDoubleX{}'.format(", MuonCR" if b'muon' in cats[0].name else "") \
             +'\n{} Region'.format("Passing" if "pass" in str(cats[0].name, 'utf-8') else "Failing")
 
-    ax.annotate( annot , linespacing=1.7,
-                xy=(0.04, 0.94), xycoords='axes fraction',  ha='left', va='top', ma='center', fontsize='small',
-                bbox={'facecolor':'white', 'edgecolor':'white', 'alpha':0, 'pad':13}, annotation_clip=False)
-
+    ax.annotate(annot,
+                linespacing=1.7,
+                xy=(0.04, 0.94),
+                xycoords='axes fraction',
+                ha='left',
+                va='top',
+                ma='center',
+                fontsize='small',
+                bbox={
+                    'facecolor': 'white',
+                    'edgecolor': 'white',
+                    'alpha': 0,
+                    'pad': 13
+                },
+                annotation_clip=False)
 
     # Leg sort
     ax.legend(*hep.plot.sort_legend(ax, label_dict), ncol=2, columnspacing=0.8)
 
-    name = str("pass" if "pass" in str(cats[0].name, 'utf-8') else "fail") + str(str(ipt) if len(cats) == 1 else "")
+    if b'muon' in cats[0].name:
+        _iptname = "MuonCR"
+    else:
+        _iptname = str(str(ipt) if len(cats) == 1 else "")
+    name = str("pass" if "pass" in str(cats[0].name, 'utf-8') else "fail"
+               ) + _iptname
 
-    fig.savefig('plots/{}.png'.format(name), transparent=True, bbox_inches = "tight")
+    fig.savefig('plots/{}.png'.format(args.namespace + "_" + name),
+                #transparent=True,
+                bbox_inches="tight")
 
 
 shape_type = 'shapes_' + args.namespace
 
-f = uproot.open('fitDiagnostics.root')
+f = uproot.open(args.input_file)
 pbins = [450, 500, 550, 600, 675, 800, 1200]
 axees = []
 for region in ['pass', 'fail']:
-    print("Plotting region {}".format(region))
-    for i in range(0,6):
+    print("Plotting {} region".format(region))
+    for i in range(0, 6):
         cat_name = 'ptbin{}{};1'.format(i, region)
-        cat = f[shape_type][cat_name]
+        try:
+            cat = f[shape_type][cat_name]
+        except:
+            raise ValueError(
+                "Namespace {} is not available, only following namespaces were found in the file: {}"
+                .format(args.namespace, f.keys()))
         full_plot([cat])
-    full_plot([f[shape_type]['ptbin{}{};1'.format(i, region)] for i in range(0,6)])
+    full_plot([f[shape_type]['ptbin{}{};1'.format(i, region)] for i in range(0, 6)])
+    # MuonCR if included
+    try:
+        cat = f[shape_type]['muonCR{};1'.format(region)]
+        full_plot([cat])
+        print("Plotted muCR")
+    except:
+        pass
---
+++ temp_Hxx.py
@@ -0,0 +1,201 @@
+from __future__ import print_function, division
+import rhalphalib as rl
+import numpy as np
+import scipy.stats
+import pickle
+import ROOT
+import uproot
+rl.util.install_roofit_helpers()
+
+def dummy_rhalphabet():
+    throwPoisson = True
+    fitTF = True
+    pseudo = False
+
+    # Default lumi (needs at least one systematics for prefit)
+    lumi = rl.NuisanceParameter('CMS_lumi', 'lnN')
+
+    # Define Bins
+    ptbins = np.array([450, 500, 550, 600, 675, 800, 1200])
+    npt = len(ptbins) - 1
+    msdbins = np.linspace(40, 201, 24)
+    msd = rl.Observable('msd', msdbins)
+
+    # Define pt/msd/rho grids
+    ptpts, msdpts = np.meshgrid(ptbins[:-1] + 0.3 * np.diff(ptbins), msdbins[:-1] + 0.5 * np.diff(msdbins), indexing='ij')
+    rhopts = 2*np.log(msdpts/ptpts)
+    ptscaled = (ptpts - 450.) / (1200. - 450.)
+    rhoscaled = (rhopts - (-6)) / ((-2.1) - (-6))
+    validbins = (rhoscaled >= 0) & (rhoscaled <= 1)
+    rhoscaled[~validbins] = 1  # we will mask these out later
+
+    # Template reading
+    def get_templ(region, sample, ptbin):
+        import uproot
+        f = uproot.open('hxx/hist_1DZcc_pt_scalesmear.root')
+        hist_name = '{}_{}'.format(sample, region)
+        h_vals = f[hist_name].values[:, ptbin]
+        h_edges = f[hist_name].edges[0]
+        h_key = 'msd'
+        return (h_vals, h_edges, h_key)
+
+
+    # Get QCD efficiency
+    qcdpass, qcdfail = 0., 0.
+    for ptbin in range(npt):
+        failCh = rl.Channel("ptbin%d%s" % (ptbin, 'fail'))
+        passCh = rl.Channel("ptbin%d%s" % (ptbin, 'pass'))
+
+        passTempl = get_templ("pass", "qcd", ptbin)
+        failTempl = get_templ("fail", "qcd", ptbin)
+
+        failCh.setObservation(failTempl)
+        passCh.setObservation(passTempl)
+        qcdfail += failCh.getObservation().sum()
+        qcdpass += passCh.getObservation().sum()
+
+    qcdeff = qcdpass / qcdfail
+
+    # build actual fit model now
+    model = rl.Model("tempModel")
+
+    # Load templates
+    f = uproot.open('hxx/hist_1DZcc_pt_scalesmear.root')
+    templates = {}
+    sys_list = ['JES']  # Sys list
+    sysud_list = sum([[sys+"Up", sys+"Down"] for sys in sys_list], [])  # Sys name list
+    for ptbin in range(npt):
+        for region in ['pass', 'fail']:
+            templates[region+str(ptbin)] = {}
+            for sample in ['zbb', 'zcc', 'zqq', 'wcq', 'wqq', 'hcc125', 'tqq', 'hqq125', 'qcd', 'data_obs']:
+                for sys_name in [""] + sysud_list:
+                    hist_name = '{}_{}'.format(sample, region)
+                    _outname = sample.replace('125', '')
+                    if len(sys_name) > 0:
+                        hist_name += "_"+sys_name
+                        _outname += "_"+sys_name
+                    h_vals = f[hist_name].values[:, ptbin]
+                    h_edges = f[hist_name].edges[0]
+                    h_key = 'msd'
+                    templates[region+str(ptbin)][_outname] = (h_vals, h_edges, h_key)
+
+    for ptbin in range(npt):
+        for region in ['pass', 'fail']:
+            ch = rl.Channel("ptbin%d%s" % (ptbin, region))
+            model.addChannel(ch)
+
+            #for sName in ['zbb', 'zcc', 'zqq', 'wcq', 'wqq', 'hcc', 'tqq', 'hqq']:
+            #include_samples = ['zcc']
+            include_samples = ['zbb', 'zcc', 'zqq', 'wcq', 'wqq', 'hcc', 'tqq', 'hqq']
+            if not fitTF:  # Add QCD sample when not running TF fit
+                include_samples.append('qcd')
+            for sName in include_samples:
+                templ = templates[region+str(ptbin)][sName]
+                stype = rl.Sample.SIGNAL if sName in ['zcc'] else rl.Sample.BACKGROUND
+                sample = rl.TemplateSample(ch.name + '_' + sName, stype, templ)
+
+                sample.setParamEffect(lumi, 1.027)
+
+                ch.addSample(sample)
+
+            if not pseudo:
+                data_obs = templates[region+str(ptbin)]['data_obs']
+
+            else:
+                yields = sum(tpl[0] for samp, tpl in templates[region+str(ptbin)].items() if  samp in [*include_samples, 'qcd'])
+                if throwPoisson:
+                    yields = np.random.poisson(yields)
+
+                data_obs = (yields, msd.binning, msd.name)
+            ch.setObservation(data_obs)
+
+            # drop bins outside rho validity
+            mask = validbins[ptbin]
+            # blind bins 11, 12, 13
+            if not pseudo:
+                mask[11:14] = False
+            ch.mask = mask
+
+    if fitTF:
+        tf_dataResidual = rl.BernsteinPoly("tf_dataResidual", (2, 2), ['pt', 'rho'], limits=(0, 10))
+        tf_dataResidual_params = tf_dataResidual(ptscaled, rhoscaled)
+        tf_params = qcdeff * tf_dataResidual_params
+
+        for ptbin in range(npt):
+            failCh = model['ptbin%dfail' % ptbin]
+            passCh = model['ptbin%dpass' % ptbin]
+
+            qcdparams = np.array([rl.IndependentParameter('qcdparam_ptbin%d_msdbin%d' % (ptbin, i), 0) for i in range(msd.nbins)])
+            initial_qcd = failCh.getObservation().astype(float)
+            # was integer, and numpy complained about subtracting float from it
+            for sample in failCh:
+                initial_qcd -= sample.getExpectation(nominal=True)
+            if np.any(initial_qcd < 0.):
+                raise ValueError("initial_qcd negative for some bins..", initial_qcd)
+            sigmascale = 10  # to scale the deviation from initial
+            scaledparams = initial_qcd * (1 + sigmascale/np.maximum(1., np.sqrt(initial_qcd)))**qcdparams
+            fail_qcd = rl.ParametericSample('ptbin%dfail_qcd' % ptbin, rl.Sample.BACKGROUND, msd, scaledparams)
+            failCh.addSample(fail_qcd)
+            pass_qcd = rl.TransferFactorSample('ptbin%dpass_qcd' % ptbin, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
+            passCh.addSample(pass_qcd)
+
+        # tqqpass = passCh['tqq']
+        # tqqfail = failCh['tqq']
+        # tqqPF = tqqpass.getExpectation(nominal=True).sum() \
+        #     / tqqfail.getExpectation(nominal=True).sum()
+        # tqqpass.setParamEffect(tqqeffSF, 1*tqqeffSF)
+        # tqqfail.setParamEffect(tqqeffSF, (1 - tqqeffSF) * tqqPF + 1)
+        # tqqpass.setParamEffect(tqqnormSF, 1*tqqnormSF)
+        # tqqfail.setParamEffect(tqqnormSF, 1*tqqnormSF)
+
+    # Fill in muon CR
+    # templates = {}
+    # for ptbin in range(npt):
+    #     for region in ['pass', 'fail']:
+    #         templates[region+str(ptbin)] = {}
+
+    # for region in ['pass', 'fail']:
+    #     ch = rl.Channel("muonCR%s" % (region, ))
+    #     model.addChannel(ch)
+
+    #     templates = {}
+    #     f = uproot.open(
+    #         'hxx/hist_1DZcc_pt_scalesmear.root')
+    #     for sample in ['tqq', 'qcd']:
+    #         hist_name = '{}_{}'.format(sample, region)
+    #         h_vals = f[hist_name].values[:, ptbin]
+    #         h_edges = f[hist_name].edges[0]
+    #         h_key = 'msd'
+    #         templates[sample.replace('125', '')] = (h_vals, h_edges, h_key)
+
+    #     for sName, templ in templates.items():
+    #         stype = rl.Sample.BACKGROUND
+    #         sample = rl.TemplateSample(ch.name + '_' + sName, stype, templ)
+
+    #         # mock systematics
+    #         jecup_ratio = np.random.normal(loc=1, scale=0.05, size=msd.nbins)
+    #         sample.setParamEffect(jec, jecup_ratio)
+
+    #         ch.addSample(sample)
+
+    #     yields = sum(tpl[0] for tpl in templates.values())
+    #     if throwPoisson:
+    #         yields = np.random.poisson(yields)
+    #     data_obs = (yields, msd.binning, msd.name)
+    #     ch.setObservation(data_obs)
+
+    # tqqpass = model['muonCRpass_tqq']
+    # tqqfail = model['muonCRfail_tqq']
+    # tqqPF = tqqpass.getExpectation(nominal=True).sum() / tqqfail.getExpectation(nominal=True).sum()
+    # tqqpass.setParamEffect(tqqeffSF, 1*tqqeffSF)
+    # tqqfail.setParamEffect(tqqeffSF, (1 - tqqeffSF) * tqqPF + 1)
+    # tqqpass.setParamEffect(tqqnormSF, 1*tqqnormSF)
+    # tqqfail.setParamEffect(tqqnormSF, 1*tqqnormSF)
+
+    with open("tempModel.pkl", "wb") as fout:
+        pickle.dump(model, fout)
+
+    model.renderCombine("tempModel")
+
+if __name__ == '__main__':
+    dummy_rhalphabet()
