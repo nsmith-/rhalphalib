cec7ab3a11906096db3f4b56637acc5b2d8ca4e8
Andrzej <novak5andrzej@gmail.com>
2019-11-21 13:23:51 -0600

    tmp morph

---
+++ template_morph.py
@@ -0,0 +1,27 @@
+from scipy.interpolate import interp1d
+import numpy as np
+
+
+class AffineMorphTemplate(object):
+    def __init__(self, hist):
+        '''
+        hist: a numpy-histogram-like tuple of (sumw, edges)
+        '''
+        self.sumw, self.edges = hist
+        self.norm = self.sumw.sum()
+        self.mean = (self.sumw*np.diff(self.edges)).sum() / self.norm
+        self.cdf = interp1d(x=self.edges,
+                            y=np.r_[0, np.cumsum(self.sumw / self.norm)],
+                            kind='linear',
+                            assume_sorted=True,
+                            bounds_error=False,
+                            fill_value=(0, 1),
+                            )
+        
+    def get(self, shift=0., scale=1.):
+        '''
+        Return a shifted and scaled histogram
+        i.e. new edges = edges * scale + shift
+        '''
+        scaled_edges = (self.edges - shift) / scale
+        return np.diff(self.cdf(scaled_edges)) * self.norm, self.edges
