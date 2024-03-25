d36b11a8317d0500097c7ca155a831241a682617
andrzejnovak <novak5andrzej@gmail.com>
2024-03-25 10:43:24 +0100

    fix: add value eval for smoothstep

--- rhalphalib/parameter.py
+++ rhalphalib/parameter.py
@@ -205,7 +205,7 @@ class DependentParameter(Parameter):
 
     @property
     def value(self):
-        return eval(self.formula().format(**{p.name: p.value for p in self.getDependents(deep=True)}))
+        return eval(self.formula().replace("_smoothstep", "").format(**{p.name: p.value for p in self.getDependents(deep=True)}))
 
     @Parameter.intermediate.setter
     def intermediate(self, val):
@@ -272,7 +272,7 @@ class SmoothStep(DependentParameter):
 
     @property
     def value(self):
-        raise NotImplementedError
+        return eval(self.formula().replace("_smoothstep", "").format(**{p.name: p.value for p in self.getDependents(deep=True)}))
 
     def formula(self, rendering=False):
         return "{" + self.name + "}"
