75e96a49bb8c9b3c076b45da35c00f8a2b7fb9f3
andrzejnovak <andrzejnovak@users.noreply.github.com>
2025-03-07 15:23:50 +0000

    deploy: 6b0b316f15dd9cc5dce412bde8f9d05fa413dcdd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.3.1.dev16+gef7a598.d20250307',
+    VERSION: '0.3.1.dev21+g6b0b316.d20250307',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
